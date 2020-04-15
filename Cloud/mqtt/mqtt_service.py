228872a7528abe8f22fce7c6a7d25807fd0740b5
Emil Nielsen <emilqz@gmail.com>
2020-04-15 14:11:59 +0200

    save location, temperature, humidity in db

--- Cloud/mqtt/mqtt_service.py
+++ Cloud/mqtt/mqtt_service.py
@@ -2,7 +2,8 @@ import time
 import paho.mqtt.client as paho
 import json
 import re
-import geolocation as geo
+import mqtt.geolocation as geo
+import database.db_context as db_context
 
 # Topics for pushing setpoint values
 TOPIC_MOTION_SETPOINT = 'hcklI67o/package/{}/setpoint/motion'
@@ -18,6 +19,7 @@ TOPIC_MACLOCATION_PUBLISH = 'hcklI67o/package/+/maclocation'
 # Topics for publishing transformed data from cloud
 TOPIC_COORDINATES_PUBLISH = 'hcklI67o/package/{}/coordinates'
 TOPIC_PACKAGEID_PUBLISH = 'hcklI67o/device/{}/package'
+
 
 class MqttService:
     """
@@ -60,21 +62,21 @@ class MqttService:
         package_id = self.__extract_package_id(msg.topic)
         if not package_id:
             return
-        
+
         if self.__matches_topic(msg.topic, TOPIC_MOTION_PUBLISH):
             pass  # TODO
         elif self.__matches_topic(msg.topic, TOPIC_TEMPERATURE_PUBLISH):
-            pass  # TODO
+            self.__handle_temperature_exceeding(package_id, msg.payload)
         elif self.__matches_topic(msg.topic, TOPIC_HUMIDITY_PUBLISH):
-            pass  # TODO
+            self.__handle_humidity_exceeding(package_id, msg.payload)
         elif self.__matches_topic(msg.topic, TOPIC_MACLOCATION_PUBLISH):
             self.__handle_mac_scan(package_id, msg.payload)
-    
+
     def __extract_package_id(self, topic):
         id_groups = re.search(r'package/(\w+)/', topic).groups()
         if len(id_groups) is not 1:
             return None
-        
+
         return id_groups[0]
 
     def __matches_topic(self, input_topic, reference_topic):
@@ -86,13 +88,18 @@ class MqttService:
         if len(json_stations) < 2:
             print("Too few, baah")
             return
-        
+
         stations = [(self.__to_mac_address(station[0]), station[1]) for station in json_stations]
         time = json_stations[0][2]
         coordinates = geo.coordinates_from_mac(stations)
 
         if coordinates:
             print('{}: {}'.format(time, coordinates))
+
+            # save location in db
+            db_context.insert_location(package_id=package_id, timestamp=time, latitude=coordinates[0],
+                                       longitude=coordinates[1],
+                                       accuracy=coordinates[2])
 
         # TODO: Publish as json to MQTT broker
 
@@ -104,6 +111,26 @@ class MqttService:
             mac += ch
         return mac
 
+    def __handle_temperature_exceeding(self, package_id, temperature_payload):
+        temperature = json.loads(temperature_payload.decode())
+
+        if temperature:
+            print("Exceeding temperature: {}".format(temperature))
+
+            # TODO: TIME
+            # save temperature in db
+            db_context.insert_temperature_exceeding(package_id=package_id, timestamp=123, temperature=temperature)
+
+    def __handle_humidity_exceeding(self, package_id, humidity_payload):
+        humidity = json.loads(humidity_payload.decode())
+
+        if humidity:
+            print("Exceeding humitidy: {}".format(humidity))
+
+            # TODO: TIME
+            # save humidity in db
+            db_context.insert_humidity_exceeding(package_id=package_id, timestamp=123, humidity=humidity)
+
     def set_motion_setpoint(self, package_id, setpoint):
         self.client.publish(TOPIC_MOTION_SETPOINT.format(package_id), setpoint, qos=1, retain=True)
 
@@ -112,6 +139,8 @@ class MqttService:
 
     def set_humidity_setpoint(self, package_id, setpoint):
         self.client.publish(TOPIC_HUMIDITY_SETPOINT.format(package_id), setpoint, qos=1, retain=True)
+
+
 
 if __name__ == '__main__':
     mqtt = MqttService()
